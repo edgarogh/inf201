25a6fa7181bdb1cdaf6d81f54c9a85ae79975ca9
Edgar Onghena <46636609+edgarogh@users.noreply.github.com>
2020-04-29 13:06:39 +0200

    :construction_worker: Add superpowers to build script

--- rummikub/build.ml
+++ rummikub/build.ml
@@ -6,38 +6,51 @@
     `ocaml build.ml start`: lancer seulement (changements pas pris en compte)
 *)
 
-(** Build / Start *)
-type task_type = B | S
+let dep_name_of_line line =
+    let len = String.length line in
+    if len < 6
+    then None
+    else if String.sub line 0 5 <> "open "
+    then None
+    else Some (String.lowercase_ascii @@ String.sub line 5 (len - 5))
 
-(* Raccourci vers `ocamlc` *)
-let c file = "ocamlc -c " ^ file ^ ".ml"
+let deps_of_file path =
+    let file = open_in path in
+    let rec next_deps () =
+        let line = try input_line file with End_of_file -> "" in
+        match dep_name_of_line line with
+        | None -> []
+        | Some dep -> dep :: next_deps ()
+    in
+    next_deps ()
 
-let tasks = [
-    B, c "multiensemble";
-    B, c "tuiles";
-    S, "ocaml multiensemble.cmo tuiles.cmo main.ml";
-]
+let file_name ext dep = String.lowercase_ascii dep ^ "." ^ ext
 
-(* Exécuteur de tâches *)
+let rec dep_list name =
+    [name] @ let deps = deps_of_file @@ file_name "ml" name in
+    List.flatten @@ List.map dep_list deps
 
-let array_get_opt arr idx =
-    try Some (Array.get arr idx)
-    with Invalid_argument _ -> None
+let filter_duplicates list =
+    List.fold_left (fun acc -> fun el -> if List.mem el acc then acc else acc @ [el]) [] list
 
-let run_command cmd =
+let () = assert ([1; 2; 3] = filter_duplicates [1; 2; 1; 2; 2; 3; 1; 3])
+
+let run cmd =
     print_endline ("$ " ^ cmd);
-    0 = Sys.command cmd
+    ignore @@ Sys.command cmd
 
-let task_types = match array_get_opt Sys.argv 1 with
-    | None | Some "" -> [B; S]
-    | Some "build" -> [B]
-    | Some "start" -> [S]
-    | _ -> failwith "Argument invalide. ``, `build` ou `start` attendu"
+let rec list_without_tail = function
+    | [] -> []
+    | last :: [] -> []
+    | el :: rest -> el :: list_without_tail rest
 
-(* Point d'entrée *)
-let () =
-    let tasks = List.filter (fun (t, cmd) -> List.mem t task_types) tasks in
-    let commands = List.map snd tasks in
-    if List.for_all run_command commands
-    then ()
-    else print_endline "* Echec"
+(* Compilation *)
+let entry_point = if Array.length Sys.argv >= 2 then Sys.argv.(1) else "main"
+let dep_list = filter_duplicates @@ List.rev @@ dep_list entry_point
+let deps ext dep_list = String.concat " " (List.map (file_name ext) dep_list)
+
+let deps_ml = deps "ml" dep_list
+let deps_cmo_wo_ep = deps "cmo" (list_without_tail dep_list)
+
+let () = run ("ocamlc -c " ^ (deps "ml" dep_list))
+let () = run ("ocaml " ^ deps_cmo_wo_ep ^ " " ^ file_name "ml" entry_point)
